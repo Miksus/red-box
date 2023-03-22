2191304b75b66e8d9958e06637dc6e2e33e8531b
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2023-03-22 22:11:24 +0100

    added decoding of single text/html content

--- redbox/tests/test_fetch.py
+++ redbox/tests/test_fetch.py
@@ -83,7 +83,8 @@ def test_set_flags_methods(IMAP4):
     assert not msg.deleted
 
 
-def test_plain_text(IMAP4):
+
+def test_single_part_text_plain(IMAP4):
     box = EmailBox(host="localhost", port=0, cls_imap=DummyGmailImap)
 
     msg = EmailMessage(uid=3, session=box.inbox.session, mailbox="INBOX")
@@ -94,5 +95,17 @@ def test_plain_text(IMAP4):
         line 2
         """
     )[1:]
+
     assert msg.html_body is None
 
+
+def test_single_part_text_html(IMAP4):
+    box = EmailBox(host="localhost", port=0, cls_imap=DummyGmailImap)
+
+    msg = EmailMessage(uid=2, session=box.inbox.session, mailbox="INBOX")
+
+    assert msg.text_body is None
+    assert msg.html_body.strip() == dedent("""
+    <html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body><div dir="ltr"><br></div></body></html>
+    """).strip()
